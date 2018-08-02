fca765e3d56f8f0b9149e070207881876004950f
Bernard Teske <info@bernardteske.de>
2018-08-02 18:20:13 +0200

    Step 03, Speichern der Daten

--- bikeStorage.sol
+++ bikeStorage.sol
@@ -1,9 +1,20 @@
 pragma solidity^0.4.0;
 
 contract BikeStorage{
+    
+    struct Bike {
+        string name;
+        string registernumber;
+        address owner;
+    }
+    
+    Bike[] bikes;
+    mapping(string => uint) ownerToBike;
 
-    function addBike(string _name, string _registernumber) returns(string, string, address) {
+    function addBike(string _name, string _registernumber) returns(uint) {
         address owner = msg.sender;
-        return (_name, _registernumber, owner);
+        uint id = bikes.push(Bike(_name, _registernumber, owner));
+        ownerToBike[_registernumber] = id;
+        return id;
     }
 }
